36e0d32749f67b82d3ecd5cd7ade565be31c8c1b
Avichai <avisvazana@gmail.com>
2018-11-18 12:43:25 -0500

    Changes to actions and reducer

--- frontend/reducers/entities/playlists_reducer.js
+++ frontend/reducers/entities/playlists_reducer.js
@@ -1,10 +1,18 @@
-import { RECEIVE_ALL_PLAYLISTS } from '../../actions/playlist_actions';
+import { RECEIVE_ALL_PLAYLISTS, RECEIVE_SINGLE_PLAYLIST, DELETE_PLAYLIST } from '../../actions/playlist_actions';
+import merge from 'lodash/merge';
 
 const playlistsReducer = (state = {}, action) => {
   Object.freeze(state);
+  let newState;
   switch(action.type){
     case RECEIVE_ALL_PLAYLISTS:
       return action.playlists;
+    case RECEIVE_SINGLE_PLAYLIST:
+      return merge({}, state, {[action.playlist.id]: action.playlist});
+    case DELETE_PLAYLIST:
+      newState = merge({}, state);
+      delete newState[action.playlistId];
+      return newState;
     default:
       return state;
   }
--- frontend/util/playlist_api_util.jsx
+++ frontend/util/playlist_api_util.jsx
@@ -4,3 +4,33 @@ export const fetchPlaylists = () => {
       url: '/api/playlists'
     })
 };
+
+export const fetchPlaylist = (id) => {
+  return $.ajax({
+      method: 'GET',
+      url: `/api/playlists/${id}`
+    })
+};
+
+export const createPlaylist = (playlist) => {
+  return $.ajax({
+      method: 'POST',
+      url: `/api/playlists`,
+      data: { playlist } 
+    })
+};
+
+export const updatePlaylist = (playlist) => {
+  return $.ajax({
+      method: 'PATCH',
+      url: `/api/playlists/${playlist.id}`,
+      data: { playlist }
+    })
+};
+
+export const deletePlaylist = (id) => {
+  return $.ajax({
+      method: 'DELETE',
+      url: `/api/playlists/${id}`
+    })
+};
